115377613505e330eadb7e039e01aa97870e8458
uzzal2427 <uzzal2427ahmed@gmail.com>
2024-07-22 12:38:04 +0300

    eslint setup done

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 import app from "./App";
 import config from "./config";
 
-const port = 5000;
-
 async function main() {
   await mongoose.connect(config.db_url as string);
 
